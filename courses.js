a566a784659a4d9b85fbe4b0fad65528a90ccabd
Migue Rubio <MIGUERUBIO01@GMAIL.COM>
2025-11-15 01:14:14 -0600

    Create courses.js

---
+++ courses.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    id: 1,
+    title: "EC0217 – Impartición de Cursos",
+    short: "Enseñanza efectiva y estructurada.",
+    description:
+      "Estándar oficial para la impartición de cursos presenciales y virtuales.",
+    duration: "40 horas",
+    mode: "Mixta"
+  },
+  {
+    id: 2,
+    title: "EC0301 – Diseño de Cursos",
+    short: "Metodología y planeación educativa.",
+    description:
+      "Domina el proceso de diseño instruccional bajo los lineamientos del CONOCER.",
+    duration: "60 horas",
+    mode: "En línea"
+  },
+  {
+    id: 3,
+    title: "EC0076 – Evaluación de Competencias",
+    short: "Evaluación bajo metodología EC.",
+    description:
+      "Dirigido a profesionales que desean acreditarse como evaluadores oficiales.",
+    duration: "50 horas",
+    mode: "Presencial"
+  }
+];
