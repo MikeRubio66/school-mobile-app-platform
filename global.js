148215f252ce7467578afcfe3cc22a2b2ad5376b
Migue Rubio <MIGUERUBIO01@GMAIL.COM>
2025-11-15 01:14:50 -0600

    Create global.js for styling components

---
+++ global.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#f5f5f5"
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#003566"
+  },
+  subtitle: {
+    fontSize: 16,
+    textAlign: "center",
+    marginTop: 10,
+    color: "#555"
+  }
+});
